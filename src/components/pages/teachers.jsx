c0b6328fd7b669fa65188d5d4c1c7fad2e676bbe
Cristian Fonseca <cristiancomas@hotmail.com>
2021-02-01 09:28:37 -0500

    Banner created

--- src/components/pages/teachers.jsx
+++ src/components/pages/teachers.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import Banner from "../organisms/banner"
 const Teachers = () => {
   return (
-    <div className="ed-grid">
-      <h1>Teachers</h1>
-    </div>
+    <Banner 
+      color="third-color" 
+      image={{
+        src: "https://blog.elink.io/wp-content/uploads/2020/03/Online-Education-Tools-For-Modern-Teachers-Blog-Banner-1250x500.png",
+        alt: "Banner Profesores"
+      }}
+      title="Our teachers"
+      subtitle="This teacher is highly qualified to guide you in your education"
+    />
   )
 }
 
